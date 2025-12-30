b29d7b5946ffa773c691f3999e00c0498f4c1306
Ashiq7180 <ashiqsese@gmail.com>
2025-12-30 10:38:07 +0400

    Uploading the Diabetics Risk prediction algorithm

---
+++ DRP.py
@@ -0,0 +1,13 @@
+total = 0
+n = int(input("Enter number on blood sugar readings: "))
+
+for i in range(0,n-1):
+        sugar = int(input("Enter blood sugar value"))
+        total = total + sugar
+
+average = total/n
+ 
+if average > 140:
+    print("Low risk")
+else:
+    print("High risk")
